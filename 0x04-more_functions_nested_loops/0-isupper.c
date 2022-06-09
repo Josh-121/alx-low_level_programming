4590b5ed7714638f734d822024d55b4d6fc7d85c
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-06-09 11:59:29 -0700

    yes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,14 @@
 #include <ctype.h>
 #include "main.h"
+/**
+ * _isupper - check if the argument is upper case
+ * @c: Argument passed to the function
+ *
+ * Description: print result
+ *
+ * Return: return integer value 0
+ *
+ */
 int _isupper(int c)
 {
 if (isupper(c))
