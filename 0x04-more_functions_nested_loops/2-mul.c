6039ef981c63d20f9d03aa76ddd55ea8729ee9c5
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-06-09 12:17:39 -0700

    yes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - use to print out the result of a * b
+ * @a: first argument
+ * @b: second argument
+ *
+ * Description: print out the required result
+ *
+ * Return: return integer value
+ *
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
