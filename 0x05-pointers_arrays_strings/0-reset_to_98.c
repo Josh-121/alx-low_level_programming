bec7b3b6edcbb33eea2563fb0317fa1ee24f91b1
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-06-13 04:08:10 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * pointers
+ * @n: number to be printed out by the function
+ *
+ * Description: return the required result
+ *
+ * Return: return integer value 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
