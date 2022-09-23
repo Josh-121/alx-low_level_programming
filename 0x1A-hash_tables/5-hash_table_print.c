58cc4d8e2470cb0a86d44f33f4e4ba0d1620e80e
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-09-23 21:05:42 +0100

    hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,15 +3,18 @@ void hash_table_print(const hash_table_t *ht)
 {
     int i = 0;
     int out = 0;
+    printf("{");
     while(i < (int)ht->size)
     {
         if(ht->array[i] != NULL)
         {
             if (out > 0)
                 printf(", ");
-            printf("{\'%s\': \'%s\'}",ht->array[i]->key,ht->array[i]->value);
+            printf("\'%s\': \'%s\'",ht->array[i]->key,ht->array[i]->value);
             out++;
         }
+
         i++ ;
     }
+    printf("}\n");
 }
